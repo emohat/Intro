8a1a3624609134700053628bd1c9cd3a055ac0cf
emohat <erin.mohat@gmail.com>
2015-06-02 21:14:47 -0600

    modified basics formatting in file.

--- basics.rb
+++ basics.rb
@@ -1,8 +1,10 @@
+#1
 first = "Erin "
 last = "Mohat"
 
 puts first + last
 
+#2
 thousands = 5218 / 1000
 puts thousands
 hundreds = 5218 % 1000 / 100
@@ -12,6 +14,7 @@ puts tens
 ones = 5218 % 10
 puts ones
 
+#3
 movies = {
   the_matrix: 1999,
   the_matrix_reloaded: 2003,
@@ -21,20 +24,24 @@ puts movies[:the_matrix]
 puts movies[:the_matrix_reloaded]
 puts movies[:the_matrix_revolutions]
 
+#4
 dates = [1999, 2003, 2004]
 puts dates[0]
 puts dates[1]
 puts dates[2]
 
+#5
 puts 5 * 4 * 3 * 2 * 1
 puts 6 * 5 * 4 * 3 * 2 * 1
 puts 7 * 6 * 5 * 4 * 3 * 2 * 1
 puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
 
+#6
 puts 5.21 * 5.21
 puts 2.18 * 2.18
 puts 1.85 * 1.85
 
+#7
 # Error message indicates that there is an additional bracket 
 # when there should be a curly bracket.  Replacing the ] with a }
 # would resolve the problem.
